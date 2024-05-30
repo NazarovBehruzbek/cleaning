79f457c1b97690ae26858a83926b92ea6c844add
Nazarov Behruzbek <behruzbeknazarov001@gmail.com>
2024-05-30 18:34:11 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import React, { useEffect } from 'react'
-import 'aos/dist/aos.css';
+import React from 'react'
+
 import './styles.css'
-import Aos from 'aos';
-import { useTranslation } from 'react-i18next';
-import Timeline from './timeline';
+
 export default function App() {
-  const {t,i18n} = useTranslation()
-  useEffect(() => {
-    Aos.init({
-      duration: 900,
-      offset: 100,
-      easing: 'ease',
-      once: true,
-    });
-  }, []);
+  
   return (
     <>
-  <Timeline/>
+
     </>
   )
 }
